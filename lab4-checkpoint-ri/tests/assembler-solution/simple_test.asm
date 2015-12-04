b6497e44e2147486c0daeb7767a0ef9e0d4865d3
Juaquin <jnikksanchez@gmail.com>
2015-12-04 13:37:26 -0500

    testing

--- lab4-checkpoint-ri/tests/assembler-solution/simple_test.asm
+++ lab4-checkpoint-ri/tests/assembler-solution/simple_test.asm
@@ -1,17 +1,15 @@
-addi $s0, $zero, 7 	#1st
-addi $s1, $zero, 9	#3rd
-addi $s2, $zero, 5	#2nd
-jr $s0
-addi $s0, $zero, 31 #011111
-jr $s1
-addi $s1, $zero, 36	#100100
-jr $s2
+addi $s0, $zero, 7
+addi $s1, $zero, 9
 addi $s2, $zero, 51	#110011
 addi $s3, $zero, 51	#110011
 bne $s2, $s3, hello
 beq $s2, $s3, world
-hello: 	addi $s0, $zero, 51	#110011 - hello is skipped
+hello: 	addi $s0, $zero, 51	#110011
 j foo
 world: 	addi $s1, $zero, 12 #001100
 jal hello
 foo: 	addi $s2, $zero, 42 #101010
+addi $s0, $zero, 0
+addi $s1, $zero, 0
+addi $s2, $zero, 0
+jr $ra
--- lab4-checkpoint-ri/tests/assembler-solution/test.asm
+++ lab4-checkpoint-ri/tests/assembler-solution/test.asm
@@ -1,6 +1,6 @@
-addi $s0, $zero, 7 	#1st
-addi $s1, $zero, 9	#3rd
-addi $s2, $zero, 5	#2nd
+addi $s0, $zero, 0x40001c 	#line 7 - 1st
+addi $s1, $zero, 0x400024	#line 9 - 3rd
+addi $s2, $zero, 0x400014	#line 5 - 2nd
 jr $s0
 addi $s0, $zero, 31 #011111
 jr $s1
@@ -10,7 +10,7 @@ addi $s2, $zero, 51	#110011
 addi $s3, $zero, 51	#110011
 bne $s2, $s3, hello
 beq $s2, $s3, world
-hello: 	addi $s0, $zero, 51	#110011 - hello is skipped
+hello: 	addi $s0, $zero, 51	#110011 - skipped initially
 j foo
 world: 	addi $s1, $zero, 12 #001100
 jal hello
